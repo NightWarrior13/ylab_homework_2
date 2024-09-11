c3fd85e88545eee73e1bb2379033eb85a3345262
Denis Kazantsev <kaz13den@gmail.com>
2024-09-11 23:32:55 +0500

    debug

---
+++ app.js
@@ -0,0 +1,34 @@
+import { createElement } from "./utils.js";
+
+function App({list}) {
+   return (
+      createElement('div',{className: 'App'},
+         createElement('div', {className: 'App-head'},
+            createElement('h1', {textContent: 'Приложение на чистом JS'})
+         ),
+         createElement('div', {className: 'App-controls'},
+            createElement('button', {textContent: 'Добавить', onclick:()=>{
+
+               }
+            })
+         ),
+         createElement('div', {className: 'App-center'},
+            createElement('div', {className: 'List'},
+               ...list.map(item=>
+                  createElement('div', {className: 'List-item'},
+                     createElement('div', {className: 'Item'+(item.selected ? ' Item_selected' : '')},
+                        createElement('div', {className: 'Item-code', textContent: item.code}),
+                        createElement('div', {className: 'Item-title', textContent: item.title}),
+                        createElement('div', {className: 'Item-actions'},
+                           createElement('button', {textContent: 'Удалить'})
+                        ),
+                     )
+                  )
+               )
+            )
+         )
+      )
+   )
+}
+
+export default App;
---
+++ main.js
@@ -0,0 +1,20 @@
+import { createElement } from "./utils.js";
+import App from "./app.js";
+
+const list = [
+     { code: 1, title: 'Название элемента' },
+     { code: 2, title: 'Некий объект' },
+     { code: 3, title: 'Заголовок' },
+     { code: 4, title: 'Очень длинное название элемента из семи слов' },
+     { code: 5, title: 'Запись', selected: true },
+     { code: 6, title: 'Шестая запись' },
+     { code: 7, title: 'Седьмая запись' },
+]
+ 
+const app = App({list});
+
+
+
+
+
+document.body.append(app);
